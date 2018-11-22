11a0ee6b4b356f0a17c7806f4c987b42cdb4d178
Aliaksandr Dubitski <alex-liberty@users.noreply.github.com>
2018-11-22 13:07:23 +0300

    so hard

--- 21vek.rb
+++ 21vek.rb
@@ -9,14 +9,13 @@ html = open(url)
 doc = Nokogiri::HTML(html)
 
 showings = doc.xpath('//ul[@class="b-result"]/li').map do |showing|
+  desc = showing.xpath('.//dd[contains (@class, "result__desc")]//tr[contains(.,"Диагональ экрана")]')
   showing = {name: showing.xpath('.//span[@class="result__name"]/text()').text,
              price: showing.xpath('.//span[@data-price]/@data-price').text,
-             code: showing.xpath('.//span[@class="g-code"]/text()').text}
-  showings = doc.xpath('//dd[@class="result__desc "]//tr').map do |showing|
-    showing = {diagon: showing.xpath('.//td[@class="result__attr_var  cr-result__attr_odd"]/text()').text,
-               diagonal: showing.xpath('.//td[@class="result__attr_val  cr-result__attr_odd"]/text()').text}
-  end
-  end
+             code: showing.xpath('.//span[@class="g-code"]/text()').text,
+             diagon: desc.xpath('.//td[contains (@class, "result__attr_var")]/text()').text,
+             diagonal: desc.xpath('.//td[contains (@class,"result__attr_val")]/text()').text}
+end
 
 
 url = doc.xpath('(//div[@id="j-paginator"]//*[@name="2"]/@href)[1]').text
