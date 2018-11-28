eab6ae21dc8750c15a1bef4696c5a8c4f5c1eabb
x3qop3 <x3qop3@gmail.com>
2018-11-28 21:17:35 +0300

    ne rabotaet

--- 21vek.rb
+++ 21vek.rb
@@ -5,48 +5,57 @@ require 'csv'
 url = 'https://www.21vek.by/mobile/'
 html = open(url)
 doc = Nokogiri::HTML(html)
-showings = doc.xpath('//ul[@class="b-result"]/li').map do |showing|
+showings = []
+doc.xpath('//ul[@class="b-result"]/li').each do |showing|
 
-  showing = {name: showing.xpath('.//span[@class="result__name"]/text()').text,
+  showings << {name: showing.xpath('.//span[@class="result__name"]/text()').text,
              price: showing.xpath('.//span[@data-price]/@data-price').text,
-            code: showing.xpath('.//span[@class="g-code"]/text()').text,
-             diagon: showing.xpath('.//tr[contains(.,"Диагональ экрана")]//td[contains  (@class,"result__attr_var")]/text()').text,
+               oldprice: showing.xpath('.//span[@data-old_price]/@data-old_price').text,
+               logo: showing.xpath('.//img[@class="result__brand"]/text()').text[/brends\/(.*)\.png/,1],
+               code: showing.xpath('.//span[@class="g-code"]/text()').text.gsub(/[^0-9\. ]/, ''),
              diagonal: showing.xpath('.//tr[contains(.,"Диагональ экрана")]//td[contains (@class,"result__attr_val")]/text()').text,
-              op: showing.xpath('.//tr[contains(.,"Оперативная память")]//td[contains (@class,"result__attr_var")]/text()').text,
              oper:showing.xpath('.//tr[contains(.,"Оперативная память")]//td[contains (@class,"result__attr_val")]/text()').text
   }
 end
 
 
-url = doc.xpath('(//div[@id="j-paginator"]//a[@rel="next"]/preceding-sibling::a[1]/text()').text
-pages = doc.xpath('(//div[@id="j-paginator"]//a[@rel="next"]/preceding-sibling::a[1]/text()').text
-pages = []
 
-for
-url in pages
+pages = doc.xpath('//div[@id="j-paginator"]//a[@rel="next"]/preceding-sibling::a[1]/text()').text.to_i
 
-  puts url
 
+for url in 2..pages
+
+  url = "https://www.21vek.by/mobile/page:#{url}/"
+  html = open(url)
+  doc = Nokogiri::HTML(html)
+  doc.xpath('//ul[@class="b-result"]/li').each do |showing|
+
+    showings << {name: showing.xpath('.//span[@class="result__name"]/text()').text,
+                price: showing.xpath('.//span[@data-price]/@data-price').text,
+                 oldprice: showing.xpath('.//span[@data-old_price]/@data-old_price').text,
+                 logo: showing.xpath('.//img[@class="result__brand"]/text()').text[/brends\/(.*)\.png/,1],
+                 code: showing.xpath('.//span[@class="g-code"]/text()').text.gsub(/[^0-9\. ]/, ''),
+                diagonal: showing.xpath('.//tr[contains(.,"Диагональ экрана")]//td[contains (@class,"result__attr_val")]/text()').text,
+                oper:showing.xpath('.//tr[contains(.,"Оперативная память")]//td[contains (@class,"result__attr_val")]/text()').text
+    }
+
+  end
 end
 
+showings.each{|s| puts s }
 puts url
 html = open(url)
 result = []
 showings = doc.xpath('//ul[@class="b-result"]/li').map do |showing|
-  result<<showing.xpath('.//span[@class="result__name"]/text()').text + " - " + showing.xpath('.//span[@data-price]/@data-price').text + " - " +showing.xpath('.//span[@class="g-code"]/text()').text
+  result<<showing.xpath('.//span[@class="result__name"]/text()').text + " - " + showing.xpath('.//span[@data-price]/@data-price').text + " - "+showing.xpath('.//span[@data-old_price]/@data-old_price').text + " - " + showing.xpath('.//span[@class="g-code"]/text()').text.gsub(/[^0-9\. ]/, '')   #gsub("[^0-9\. ]")
 end
 html = open(url)
 doc = Nokogiri::HTML(html)
 puts "____________________________________________________________"
 showings = doc.xpath('//dd[contains (@class, "result__desc")]//tr[contains(.,"Диагональ экрана")]').map do |showing|
   showings = doc.xpath('//dd[contains (@class, "result__desc")]//tr[contains(.,"Оперативная память")]').map do |showing|
-  puts showing.xpath('.//td[contains (@class, "result__attr_var")]/text()').text + " - " + showing.xpath('.//td[contains (@class,"result__attr_val")]/text()').text + " - " + showing.xpath('.//td[contains (@class,"result__attr_var")]/text()').text + " - "+ showing.xpath('.//td[contains (@class,"result__attr_val")]/text()').text
-puts result .inspect
-  end
-  end
+  result<<showing.xpath('.//td[contains (@class, "result__attr_var")]/text()').text + " - " + showing.xpath('.//td[contains (@class,"result__attr_val")]/text()').text + " - " + showing.xpath('.//td[contains (@class,"result__attr_var")]/text()').text + " - "+ showing.xpath('.//td[contains (@class,"result__attr_val")]/text()').text + " - " + showing.xpath('.//img[@class="result__brand"]/text()').text[/brends\/(.*)\.png/,1]
 
-#document.at('table').search('dd').each {|row|
- # cells = row.search('dd, td').map {|cell| cell.text.strip}
 
-  #puts CSV.generate_line(cells)
-#}
+  end
+end
